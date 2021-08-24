4f3d098d09e1388d68c2b6097ab2ebd08c097b31
SarathArrepu <89147362+SarathArrepu@users.noreply.github.com>
2021-08-24 16:44:05 -0500

    updated

--- ZooSimulation.java
+++ ZooSimulation.java
@@ -11,16 +11,9 @@ public class ZooSimulation {
     Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
     System.out.print("Enter Animal name- ");  
     String str= sc.nextLine();   
-    String[] animals1 = {"tiger"};
-    String c = "tiger";
     System.out.println(greeting.toUpperCase());
-    if (str == animals1[0]){
-    System.out.println(c);}
-     else{
-      System.out.println("I am not a tiger");
-     }
-    //System.out.println(greeting.toUpperCase());
     System.out.println("The animal is = " +str);  
+    
       Tiger tommy = new Tiger();
       tommy.speak();
       tommy.move();
